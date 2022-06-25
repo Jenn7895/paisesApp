c7cfd083d8df52ebc7373fc57ece64bc4a5ee766
Jenn_07 <cuevasjennifer07@gmail.com>
2022-06-25 11:13:29 -0500

    fisrt commit

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+
+  termino : string = '';
+  hayError: boolean = false;
+  paises  : Country[] = [];
+
+  constructor( private paisService: PaisService) { }
+
+  buscar(){
+    this.hayError = false;
+    console.log(this.termino);
+    
+    this.paisService.buscarPais( this.termino )
+      .subscribe( 
+        {
+          next: paises => {
+            console.log(paises);
+            this.paises = paises;
+          },
+          
+          error: (err) => {
+            this.hayError = true;
+            this.paises   = [];
+          }
+            
+          
+        });  
+  }
+
+}
